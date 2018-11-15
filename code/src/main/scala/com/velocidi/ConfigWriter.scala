e738fb4a3fade264e290b10cd2d52d0ce56286b8
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-11-15 23:08:59 +0000

    Add basic writers

--- code/src/main/scala/com/velocidi/ConfigWriter.scala
+++ code/src/main/scala/com/velocidi/ConfigWriter.scala
@@ -1,16 +1,38 @@
 package com.velocidi
 
-import com.typesafe.config.ConfigValue
+import com.typesafe.config.{ ConfigValue, ConfigValueFactory }
 
 trait ConfigWriter[A] {
   def write(value: A): ConfigValue
 }
 
-object ConfigWriter {
+object ConfigWriter extends BasicWriters {
   object Ops {
     implicit class ConfigWriterOps[A: ConfigWriter](x: A) {
       def toConfig: ConfigValue =
         implicitly[ConfigWriter[A]].write(x)
     }
+  }
+}
+
+trait BasicWriters {
+  implicit val intWriter: ConfigWriter[Int] = new ConfigWriter[Int] {
+    def write(value: Int): ConfigValue = ConfigValueFactory.fromAnyRef(value)
+  }
+
+  implicit val longWriter: ConfigWriter[Long] = new ConfigWriter[Long] {
+    def write(value: Long): ConfigValue = ConfigValueFactory.fromAnyRef(value)
+  }
+
+  implicit val doubleWriter: ConfigWriter[Double] = new ConfigWriter[Double] {
+    def write(value: Double): ConfigValue = ConfigValueFactory.fromAnyRef(value)
+  }
+
+  implicit val stringWriter: ConfigWriter[String] = new ConfigWriter[String] {
+    def write(value: String): ConfigValue = ConfigValueFactory.fromAnyRef(value)
+  }
+
+  implicit val booleanWriter: ConfigWriter[Boolean] = new ConfigWriter[Boolean] {
+    def write(value: Boolean): ConfigValue = ConfigValueFactory.fromAnyRef(value)
   }
 }
--- code/src/main/scala/com/velocidi/Main.scala
+++ code/src/main/scala/com/velocidi/Main.scala
@@ -1,7 +1,9 @@
 package com.velocidi
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ ConfigFactory, ConfigValueFactory }
+
 import ConfigReader.Ops._
+import ConfigWriter.Ops._
 
 object Main extends App {
   val conf = ConfigFactory.parseString(
@@ -18,4 +20,10 @@ object Main extends App {
   assert(conf.getValue("c").as[Double] == 4.5)
   assert(conf.getValue("d").as[String] == "str")
   assert(conf.getValue("e").as[Boolean] == false)
+
+  assert(1.toConfig == ConfigValueFactory.fromAnyRef(1))
+  assert(1099511627776l.toConfig == ConfigValueFactory.fromAnyRef(1099511627776l))
+  assert(4.5.toConfig == ConfigValueFactory.fromAnyRef(4.5))
+  assert("str".toConfig == ConfigValueFactory.fromAnyRef("str"))
+  assert(false.toConfig == ConfigValueFactory.fromAnyRef(false))
 }
